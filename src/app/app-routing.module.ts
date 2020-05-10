8358321eca38685d5f8a972acb0e42ed83db0082
omelnour <michaelwagih199@gmail.com>
2020-05-10 11:14:23 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { AuthGaurdService } from './services/auth-gaurd.service';
+import { PharmaciesComponent } from './pages/pharmacies/pharmacies.component';
+import { PatientComponent } from './pages/allPatient/patient.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: LoginComponent },//canActivate, RouteGuardService
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent, canActivate:[AuthGaurdService]},
+  { path: 'pharmacies', component: PharmaciesComponent, canActivate:[AuthGaurdService]},
+  { path: 'patients', component: PatientComponent, canActivate:[AuthGaurdService]},
+  { path: 'patients', component: PatientComponent, canActivate:[AuthGaurdService]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { LoginComponent } from './pages/login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { MenueComponent } from './pages/menu/menue.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { PharmaciesComponent } from './pages/pharmacies/pharmacies.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { PatientComponent } from './pages/allPatient/patient.component';
+import { AddPatientComponent } from './pages/add-patient/add-patient.component';
+
+
+registerLocaleData(en);
 
 @NgModule({
+
   declarations: [
-    AppComponent
+
+    AppComponent,
+    LoginComponent,
+    WelcomeComponent,
+    MenueComponent,
+    PharmaciesComponent,
+    ErrorComponent,
+    AddPatientComponent,
+    PatientComponent,
+
   ],
+
   imports: [
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzFormModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
+
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
